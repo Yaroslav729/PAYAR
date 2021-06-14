53551cbfb2cb51c2425f44ddded715263b678133
Yaroslav729 <company-rosstan@yandex.ru>
2021-06-14 22:27:55 +0300

    create: Block-1, media

--- index.php
+++ index.php
@@ -13,45 +13,97 @@
     <link rel="stylesheet" href="sass/style.css">
   </head>
   <body>
-    <header class="navbar">
-      <div class="container">
-        <div class="navbar-top">
-          <div class="navbar-top__left">
-            <a href="index.php" class="logo">
-              <img src="img/logo/logo_top.svg" alt="PAYAR" class="logo__img">
-            </a>
-            <span class="navbar-top__text">Окна и витражи любой&nbsp;сложности</span>
+    <div class="fon">
+      <header class="navbar">
+        <div class="container">
+          <div class="navbar-top">
+            <div class="navbar-top__left">
+              <a href="index.php" class="logo">
+                <img src="img/logo/logo_top.svg" alt="PAYAR" class="logo__img">
+              </a>
+              <span class="navbar-top__text">Окна и витражи любой&nbsp;сложности</span>
+            </div>
+            <!-- /.navbar-top__right -->
+            <div class="navbar-top__right">
+              <img src="img/icon/icon-telephone.svg" alt="icon: home" class="navbar__icon-telephone">
+              <a href="tel:+79139039199" class="telephone">+7 913 903 91 99</a>
+            </div>
+            <!-- /.navbar-top__left -->
+            <button class="menu-button navbar-top__menu-button">
+              <span class="menu-button__line"></span>
+              <span class="menu-button__line"></span>
+              <span class="menu-button__line"></span>
+            </button>
           </div>
-          <!-- /.navbar-top__right -->
-          <div class="navbar-top__right">
-            <img src="img/icon/icon-telephone.svg" alt="icon: home" class="navbar__icon-telephone">
-            <a href="tel:+79139039199" class="telephone">+7 913 903 91 99</a>
-          </div>
-          <!-- /.navbar-top__left -->
-          <button class="menu-button navbar-top__menu-button">
-            <span class="menu-button__line"></span>
-            <span class="menu-button__line"></span>
-            <span class="menu-button__line"></span>
-          </button>
-        </div>
-        <!-- /.navbar-top -->
-      </div>
-      <!-- /.container -->
-      <div class="navbar-bottom">
-        <div class="container container-bottom">
-          <ul class="navbar-menu">
-            <li class="navbar-menu__item"><a href="#" class="navbar-menu__link">О компании</a></li>
-            <li class="navbar-menu__item"><a href="#" class="navbar-menu__link">Наши работы</a></li>
-            <li class="navbar-menu__item"><a href="#" class="navbar-menu__link">Преимущества</a></li>
-            <li class="navbar-menu__item"><a href="#" class="navbar-menu__link">Отзывы</a></li>
-            <li class="navbar-menu__item"><a href="#" class="navbar-menu__link">Контакты</a></li>
-          </ul>
-          <!-- /.navbar-menu -->
+          <!-- /.navbar-top -->
         </div>
         <!-- /.container -->
-      </div>
-      <!-- /.navbar-bottom -->
-    </header>
+        <div class="navbar-bottom">
+          <div class="container container-bottom">
+            <ul class="navbar-menu">
+              <li class="navbar-menu__item"><a href="#" class="navbar-menu__link">О компании</a></li>
+              <li class="navbar-menu__item"><a href="#" class="navbar-menu__link">Наши работы</a></li>
+              <li class="navbar-menu__item"><a href="#" class="navbar-menu__link">Преимущества</a></li>
+              <li class="navbar-menu__item"><a href="#" class="navbar-menu__link">Отзывы</a></li>
+              <li class="navbar-menu__item"><a href="#" class="navbar-menu__link">Контакты</a></li>
+            </ul>
+            <!-- /.navbar-menu -->
+          </div>
+          <!-- /.container -->
+        </div>
+        <!-- /.navbar-bottom -->
+      </header>
+      <section class="offer">
+        <div class="container">
+          <div class="offer-container">
+            <div class="offer-service">
+              <h1 class="offer-service__if">Меняете окна в квартире?
+                Сделайте это <span class="offer-service__text-information">с помощью профессионалов!</span></h1>
+              <div class="offer-service__information">
+                <img src="img/icon/check.svg" alt="icon: chek" class="offer-service__chek">
+                <p class="offer-service__margin">
+                  Мы проследим, чтобы окна были <span class="offer-service__text-information">установлены грамотно и служили десятки лет.</span>
+                </p>
+              </div>
+              <div class="offer-service__information">
+                <img src="img/icon/check.svg" alt="icon: chek" class="offer-service__chek">
+                <p class="offer-service__margin">
+                  Вся наша продукция сертифицирована, всем клиентам предоставляем
+                  <span class="offer-service__text-information">гарантию до 5 лет и доставку.</span>
+                </p>
+              </div>
+              <div class="offer-service__information">
+                <img src="img/icon/check.svg" alt="icon: chek" class="offer-service__chek">
+                <p class="offer-service__margin">
+                  <span class="offer-service__text-information">Запишитесь на замер</span> на сайте.
+                </p>
+              </div>
+            </div>
+            <!-- /.offer-serves -->
+            <div class="offer-form">
+              <div class="offer-form__frame">
+                <div class="offer-form__border">
+                  <h3 class="offer-form__text">Заполните форму</h3>
+                  <input type="text" class="offer-form__name" placeholder="Ваше Имя">
+                  <input type="tel" class="offer-form__phone" placeholder="Ваш телефон">
+                  <button class="button button-offer">Получить консультацию</button>
+                  <p class="offer-form__understanding">
+                    Нажимая на кнопку, вы даете согласие на обработку&nbsp;персональных данных и соглашаетесь
+                    c&nbsp;<span class="offer-form__understanding-text">политикой&nbsp;конфиденциальности.</span>
+                  </p>
+                </div>
+                <!-- /.offer-form__border -->
+              </div>
+              <!-- /.offer-form__frame -->
+            </div>
+            <!-- /.offer-form -->
+          </div>
+          <!-- /.offer-container -->
+        </div>
+        <!-- /.container -->
+      </section>
+    </div>
+    <!-- /.fon -->
 
 
   </body>
