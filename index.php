b316b6108f946b6b95e8a1c997d73e27cb2d08f8
Yaroslav729 <company-rosstan@yandex.ru>
2021-06-14 22:27:55 +0300

    create: Header, media

---
+++ index.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Окoнные конструкции в Бердске</title>
+    <link rel="apple-touch-icon" sizes="76x76" href="apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600&family=Noto+Sans:wght@400;700&family=Ubuntu:wght@500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="sass/style.css">
+  </head>
+  <body>
+    <header class="navbar">
+      <div class="container">
+        <div class="navbar-top">
+          <div class="navbar-top__left">
+            <a href="index.php" class="logo">
+              <img src="img/logo/logo_top.svg" alt="PAYAR" class="logo__img">
+            </a>
+            <span class="navbar-top__text">Окна и витражи любой&nbsp;сложности</span>
+          </div>
+          <!-- /.navbar-top__right -->
+          <div class="navbar-top__right">
+            <img src="img/icon/icon-telephone.svg" alt="icon: home" class="navbar__icon-telephone">
+            <a href="tel:+79139039199" class="telephone">+7 913 903 91 99</a>
+          </div>
+          <!-- /.navbar-top__left -->
+          <button class="menu-button navbar-top__menu-button">
+            <span class="menu-button__line"></span>
+            <span class="menu-button__line"></span>
+            <span class="menu-button__line"></span>
+          </button>
+        </div>
+        <!-- /.navbar-top -->
+      </div>
+      <!-- /.container -->
+      <div class="navbar-bottom">
+        <div class="container container-bottom">
+          <ul class="navbar-menu">
+            <li class="navbar-menu__item"><a href="#" class="navbar-menu__link">О компании</a></li>
+            <li class="navbar-menu__item"><a href="#" class="navbar-menu__link">Наши работы</a></li>
+            <li class="navbar-menu__item"><a href="#" class="navbar-menu__link">Преимущества</a></li>
+            <li class="navbar-menu__item"><a href="#" class="navbar-menu__link">Отзывы</a></li>
+            <li class="navbar-menu__item"><a href="#" class="navbar-menu__link">Контакты</a></li>
+          </ul>
+          <!-- /.navbar-menu -->
+        </div>
+        <!-- /.container -->
+      </div>
+      <!-- /.navbar-bottom -->
+    </header>
+
+
+  </body>
+</html>
